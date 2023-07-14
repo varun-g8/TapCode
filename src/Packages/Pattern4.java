fbe4427a7d83139e28dfb3d8e251ab88660d1bcf
varun-g8 <varunkarthik160@gmail.com>
2023-07-14 23:00:41 +0530

    first commit

---
+++ src/Packages/Pattern4.java
@@ -0,0 +1,15 @@
+package Packages;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int k = 1; k <= 5 - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Packages/Pattern6.java
@@ -0,0 +1,27 @@
+/*
+ *  * 
+   * * 
+  *   * 
+ *     * 
+* * * * * 
+ */
+
+package Packages;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int k = 1; k <= 5 - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i == 5 || j == 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
