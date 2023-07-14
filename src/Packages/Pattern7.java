6c9a40147b8533595f149bc15ebd27616b181d7a
varun-g8 <varunkarthik160@gmail.com>
2023-07-14 23:04:17 +0530

    changed output

--- src/Packages/Pattern7.java
+++ src/Packages/Pattern7.java
@@ -2,6 +2,6 @@ package Packages;
 
 public class Pattern7 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world from sandy");
     }
 }
